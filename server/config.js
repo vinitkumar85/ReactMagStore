ce009339f14369c535d45b1227889a833eade09e
Vinit Kumar <vinit.k.85@gmail.com>
2019-03-25 13:30:32 +0530

    chnaged path in config for backend

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    basePath : "http://localhost:8888/shop/backend/",
+    basePath : "http://10.151.25.172:8888/shop/backend/",
     secretToken: "tlb5vy2barxkh8pgvlpevxr5b62nwmps" 
 }
