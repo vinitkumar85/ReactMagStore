667e8338de0f671a52987cc6c69b9fd038c6cda5
Vinit Kumar <vinit.k.85@gmail.com>
2019-03-25 16:28:15 +0530

    chnaged path in config for backend

--- server/config.js
+++ server/config.js
@@ -1,4 +1,10 @@
-module.exports = {
-    basePath : "http://10.151.25.172:8888/shop/backend/",
+/* module.exports = {
+    basePath : "http://localhost:8888/shop/backend/",
     secretToken: "tlb5vy2barxkh8pgvlpevxr5b62nwmps" 
+} */
+
+
+module.exports = {
+    basePath : "http://shubhkit.hypernode.io/",
+    secretToken: "99hni9rt40qct5zyvgovjhs14w48itl5" 
 }
