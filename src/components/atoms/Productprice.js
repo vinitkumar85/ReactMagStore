728b175525e60c37b967edf70ac597689708e3e5
Vinit Kumar <vinit.k.85@gmail.com>
2019-08-01 18:59:21 +0530

    price font

--- src/components/atoms/Productprice.js
+++ src/components/atoms/Productprice.js
@@ -6,7 +6,7 @@ const Productprice = (props) => {
 {/*             {props.productPriceData.productCost && <div className="item__price--lt">MRP <i class="fas fa-rupee-sign"></i> {props.productPriceData.productCost}</div>} */}
             {props.productPriceData.productSP && <span className="item__price--dk"><i class="fas fa-rupee-sign"></i> {props.productPriceData.productSP}</span>} 
             {props.productPriceData.productCost && <span className="item__price--dt"><span><i class="fas fa-rupee-sign"></i> {props.productPriceData.productCost}</span> <em> {(((props.productPriceData.productCost - props.productPriceData.productSP) / 50.00) * 100)}% off </em></span>}
-            {!props.productPriceData.productSP && <div className="item__price--dk"><i class="fas fa-rupee-sign"></i> {props.productPrice} <span>(inclusive of all taxs)</span> </div>}
+            {!props.productPriceData.productSP && <div className="item__price--dk"><i class="fas fa-rupee-sign"></i> {props.productPrice} </div>}
         </div>
     )
 }
